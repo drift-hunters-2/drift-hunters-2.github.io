d389a7dd5c4f9419d195f118164618bd04f0e706
drift-hunters-2 <snakegame740+web@gmail.com>
2025-02-22 12:57:47 +0500

    Update custom-isotop.js

--- assets/js/custom-isotop.js
+++ assets/js/custom-isotop.js
@@ -11,22 +11,3 @@ function initializePwaFeatures() {
     registerServiceWorker();
     setupPwaInstallation();
 }
-
-
-// Set up Google Analytics
-function initializeGoogleAnalytics() {
-    const googleAnalyticsScript = document.createElement('script');
-    googleAnalyticsScript.async = true;
-    googleAnalyticsScript.src = "https://www.googletagmanager.com/gtag/js?id=G-6BPGNZNTLZ";
-    document.head.appendChild(googleAnalyticsScript);
-
-    googleAnalyticsScript.onload = function () {
-        window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('js', new Date());
-        gtag('config', 'G-6BPGNZNTLZ');
-        console.log('Google Analytics initialized.');
-    };
-}
-
-
