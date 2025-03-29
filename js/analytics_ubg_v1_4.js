83db4c620f4724e67726404c2ca6d81db5c8b1a9
drift-hunters-2 <snakegame740+web@gmail.com>
2025-03-29 11:18:40 +0500

    Update analytics_ubg_v1_4.js

--- js/analytics_ubg_v1_4.js
+++ js/analytics_ubg_v1_4.js
@@ -1,24 +1,18 @@
 function loadGoogleAnalytics(id) {
-    // Google tag (gtag.js)
-    var firstScript= document.getElementsByTagName("script")[0];
-    newScript= document.createElement("script");
-    newScript.async= "";
-    newScript.src= "https://www.googletagmanager.com/gtag/js?id="+ id;
-    firstScript.parentNode.insertBefore(newScript, firstScript);
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=" + id;
+    document.head.appendChild(script);
 
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', id);
+    script.onload = function () {
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+        gtag('config', id);
+    };
 }
 
-window.addEventListener("load", function() {
-    if (navigator.webdriver) {
-      console.log('Bot Browser');
-      loadGoogleAnalytics("G-LE1ZGTPC77");
-
-    } else {
-      console.log('Human Browser');
-      loadGoogleAnalytics("G-E7D3EVY6HR");
-    }
+window.addEventListener("load", function () {
+    console.log(navigator.webdriver ? 'Bot Browser' : 'Human Browser');
+    loadGoogleAnalytics("G-X93T1595HB");
 });
